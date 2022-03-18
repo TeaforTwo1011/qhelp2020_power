75ee369eac60948955a386461857d7471c12d84c
levolz <42385806+levolz@users.noreply.github.com>
2022-03-18 16:47:16 +0100

    simpleReg
    working example of simple regression

--- server.R
+++ server.R
@@ -1,7 +1,11 @@
-# library()
+library(shiny)
 
 # source(helpers.r)
+source("simpleReg.r")
 
 server <- function(input, output){
 
+    sim <- reactive(getPower(simpleReg_fit(n = input$n, alpha = input$alpha, beta = input$beta, err = input$err, x_mean = input$x_mean, x_sd = input$x_sd)))
+    output$simpleReg_power <- renderText(paste0("The power is: ", sim()[[1]], " (sd = ", sim()[[2]], ")"))
+    output$simpleReg_pplot <- renderPlot(plot(sim()[[3]], ylab = "p-values"))
 }
--- ui.r
+++ ui.r
@@ -1,6 +1,28 @@
 ui <- fluidPage(
     tabsetPanel(
-        tabPanel("tab1", ),
+        tabPanel("Regression",
+                 sidebarPanel(
+                     h3("Simple Regression"),
+                     h5("Sample"),
+                     numericInput("n", label = "sample size",
+                                  value = 30, min = 1, max = 1000),
+                     numericInput("x_mean", label = "mean of x",
+                                  value = 0, min = 1, max = 1000),
+                     numericInput("x_sd", label = "standard deviation of x",
+                                  value = 1, min = 1, max = 1000),
+                     h5("Parameters"),
+                     numericInput("alpha", label = "intercept",
+                                  value = 0, min = -1000, max = 1000, step = 0.1),
+                     numericInput("beta", label = "beta weight",
+                                  value = 0, min = -1000, max = 1000, step = 0.1),
+                     numericInput("err", label = "error term (deviation)",
+                                  value = 1, min = 0, max = 1000, step = 0.1)
+                 ),
+                 mainPanel(
+                     textOutput("simpleReg_power"),
+                     plotOutput("simpleReg_pplot")
+                 )
+        ),
         tabPanel("tab2", ),
         tabPanel("tab3")
     )
